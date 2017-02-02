2679c5c97c6f2f772f7c7add79be20cc5a34f269
Nicolas Gonzalez <nicolas.gonzalez@moove-it.com>
2017-02-02 13:25:43 -0300

    Fix mock pets repository to return mocked list of pets

--- app/src/test/java/com/mooveit/android/androidtemplateproject/common/mock/repository/MockPetsRepositoryImpl.java
+++ app/src/test/java/com/mooveit/android/androidtemplateproject/common/mock/repository/MockPetsRepositoryImpl.java
@@ -3,16 +3,30 @@ package com.mooveit.android.androidtemplateproject.common.mock.repository;
 import com.mooveit.android.androidtemplateproject.common.model.entities.Pet;
 import com.mooveit.android.androidtemplateproject.common.model.repository.PetsRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import rx.Observable;
 import rx.Single;
 
-public abstract class MockPetsRepositoryImpl implements PetsRepository {
+public class MockPetsRepositoryImpl implements PetsRepository {
+
+    @Inject
+    public MockPetsRepositoryImpl() {
+    }
 
     @Override
     public Observable<List<Pet>> getPets() {
-        return null;
+
+        List<Pet> pets = new ArrayList<Pet>() {{
+            add(new Pet(1, "Pepe", "available", 12345, 12345));
+            add(new Pet(2, "Jorge", "available", 67890, 67890));
+            add(new Pet(2, "Jack", "available", 934857, 28321));
+        }};
+
+        return Observable.just(pets);
     }
 
     @Override
@@ -28,5 +42,19 @@ public abstract class MockPetsRepositoryImpl implements PetsRepository {
     @Override
     public Observable<Void> deletePet(Pet pet) {
         return null;
+    }
+
+    @Override
+    public Observable<Void> deleteAll() {
+        return null;
+    }
+
+    @Override
+    public Observable<Pet> getPet(long petId) {
+        return null;
+    }
+
+    @Override
+    public void invalidateCache() {
     }
 }
